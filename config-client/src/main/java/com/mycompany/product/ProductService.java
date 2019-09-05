b5843e39d20846bc6b0bb2c6c23976ff79723210
eduardo costa <educostabra@gmail.com>
2019-09-04 22:28:38 -0300

    Added capability to refresh properties on the fly

--- config-client/src/main/java/com/mycompany/product/ProductService.java
+++ config-client/src/main/java/com/mycompany/product/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.mycompany.product.exceptions.BadRequestException;
 /**
  * The microservice responsible for implementing the services and APIs
  */
+@RefreshScope
 @RestController
 public class ProductService {
 
